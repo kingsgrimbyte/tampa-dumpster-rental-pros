0e1f227452e43365af3a4d50f58f163d8027b461
kingsgrimbyte <kingsgrimbyte@gmail.com>
2025-08-26 07:01:11 +0000

    new update today_data

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -6,7 +6,6 @@ import Faq from "./Faq";
 import Service from "@/app/components/Home/Service";
 import Affordable from "./Affordable";
 import ProcessWidget from "../Widgets/ProcessWidget";
-import AreaWeServe from "../Widgets/AreaWeServe";
 import ReviewWidget from "../Widgets/ReviewWidget";
 import Navbar from "../Navbar";
 import Link from "next/link";
@@ -22,8 +21,31 @@ const content: any = SubdomainContent.subdomainData;
 const Hero = () => {
   const cityData: any = content;
   const slugs: any = Object.keys(cityData).map((key) => cityData[key]);
+   const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    mainEntity: homeData.faq.map((faq: any) => ({
+      "@type": "Question",
+      name: faq?.FAQ?.split("[location]").join(
+        ContactInfo.location.split(",")[0].trim(),
+      ),
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: faq?.Answer?.split("[location]").join(
+          ContactInfo.location.split(",")[0].trim(),
+        ),
+      },
+    })),
+  };
+   
   return (
     <div className="">
+      {jsonLd && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      )}
       <Navbar />
       <div className="w-screen overflow-hidden  md:flex md:w-full md:flex-col md:items-center md:justify-center">
         <div className="w-full overflow-hidden text-lg  print:hidden  dark:bg-white dark:text-black">
@@ -80,14 +102,14 @@ const Hero = () => {
               <Image
                 height={10000}
                 width={10000}
-                src={`${homeData.h2Image}`}
+                src={`${homeData.h3Image}`}
                 unoptimized={true}
                 className=" h-full w-full rounded-lg object-cover shadow-lg"
                 alt={
-                  homeData.h2Image.split("/").pop()?.split(".")[0] || "image"
+                  homeData.h3Image.split("/").pop()?.split(".")[0] || "image"
                 }
                 title={
-                  homeData.h2Image.split("/").pop()?.split(".")[0] || "image"
+                  homeData.h3Image.split("/").pop()?.split(".")[0] || "image"
                 }
               />
             </div>
@@ -146,7 +168,7 @@ const Hero = () => {
           <div className="mt-14 md:mt-20"></div>
           {/* CTA */}
           {/* FAQ */}
-          <Faq />
+          <Faq data={homeData?.faq}/>
           {/* FAQ */}
           {/* Review */}
           <ReviewWidget />
